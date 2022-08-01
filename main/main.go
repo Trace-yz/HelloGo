ce0a3b83eed182647366ba1afadc3657bcebc01a
Trace-yz <po669977@163.com>
2022-08-01 17:41:30 +0800

    frist commit

---
+++ main/main.go
@@ -0,0 +1,25 @@
+package main     
+import (
+    "fmt"
+	"github.com/Trace-yz/HelloGo/model"
+	"github.com/gin-gonic/gin"
+)
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func main() {
+    var i int = 10
+	i = 30
+	i = 50
+	fmt.Println("i=",i)
+	
+	fmt.Println(model.HeroName)
+
+	r := gin.Default()
+	r.GET("/ping", ping)
+	r.Run()
+}
